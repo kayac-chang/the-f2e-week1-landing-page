44ae80b145e1c955e509f99ca7613bb8e6a27873
kayac-chang <egg734631@gmail.com>
2022-11-02 22:40:29 +0800

    feat: 🎸 header nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,23 @@
 import SVG from "~/components/SVG";
+import { RemoveScroll } from "react-remove-scroll";
+import { useCallback, useState } from "react";
+import { not } from "ramda";
+
+function useToggle() {
+  const [flag, setFlag] = useState(false);
+
+  const toggle = useCallback(() => setFlag(not), [setFlag]);
+
+  return [flag, toggle] as const;
+}
 
 function Header() {
+  const [isMenuOpen, toggleMenu] = useToggle();
   return (
-    <header className="fixed top-0 left-0 z-10 w-full bg-neutral-5">
+    <header className="ch fixed top-0 left-0 z-10 w-full bg-neutral-5">
       <div className="flex items-center px-4 py-5">
         {/* menu */}
-        <button aria-label="open menu" type="button">
+        <button aria-label="open menu" type="button" onClick={toggleMenu}>
           <SVG className="s-8" src={require("~/assets/icon/menu.svg")} />
         </button>
 
@@ -15,10 +27,45 @@ function Header() {
         </div>
 
         {/* register */}
-        <button type="button" className="ch ml-auto py-1.5 px-4 border">
+        <button type="button" className="ml-auto border py-1.5 px-4">
           立即報名
         </button>
       </div>
+
+      {isMenuOpen && (
+        <RemoveScroll>
+          <div className="fixed inset-0 -z-10 bg-black/25" />
+          <nav className="h-full w-full px-4">
+            <div className="flex py-2">
+              <button className="ml-auto" onClick={toggleMenu}>
+                <SVG className="w-8" src={require("~/assets/icon/close.svg")} />
+              </button>
+            </div>
+            <ul className="flex flex-col gap-8 pb-8 text-center">
+              <li>
+                <a className="text-neutral-2" href="/">
+                  關卡資訊
+                </a>
+              </li>
+              <li>
+                <a className="text-neutral-2" href="/">
+                  攻略資源
+                </a>
+              </li>
+              <li>
+                <a className="text-neutral-2" href="/">
+                  求職專區
+                </a>
+              </li>
+              <li>
+                <a href="/" className="button block border py-2 text-center">
+                  登入
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </RemoveScroll>
+      )}
     </header>
   );
 }
